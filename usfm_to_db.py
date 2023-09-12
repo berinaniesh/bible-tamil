d7a0d5186c316600a8fbed30214d3e5f7f5229c9
Berin Aniesh <berinaniesh@gmail.com>
2023-09-12 17:33:12 +0530

    fix errors and update script

--- usfm_to_db.py
+++ usfm_to_db.py
@@ -10,7 +10,15 @@ def read_file(book):
     f.close()
     return s
 
+def get_full_english_name(text):
+    texts = text.split("\n")
+    for i in texts:
+        if i.startswith(r"\mt3"):
+            return i[5:]
+
+
 books = glob.glob("./usfm/*")
-book_names = [get_book_number_and_name(i) for i in books]
 book_texts = [read_file(i) for i in books]
+book_names = [get_book_number_and_name(i) for i in books]
+book_full_english_names = [get_full_english_name(i) for i in book_texts]
 
