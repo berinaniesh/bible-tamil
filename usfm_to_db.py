cbd6203274294ffb7daaa71e34dff7aa2eccc712
Berin Aniesh <berinaniesh@gmail.com>
2023-09-12 17:47:16 +0530

    update

--- usfm_to_db.py
+++ usfm_to_db.py
@@ -17,11 +17,26 @@ def get_full_english_name(text):
             return i[5:]
 
 
+def get_full_tam_name(text):
+    texts = text.split("\n")
+    titles = []
+    for i in texts:
+        if i.startswith(r"\mt1"):
+            titles.append(i[5:])
+            break
+    for i in texts:
+        if i.startswith(r"\mt2"):
+            titles.append(i[5:])
+            break
+    return " ".join(titles)
+
 books = glob.glob("./usfm/*")
 books.sort()
 book_texts = [read_file(i) for i in books]
 book_names = [get_book_name(i) for i in books]
 book_full_english_names = [get_full_english_name(i) for i in book_texts]
 
-for i, val in enumerate(book_names):
-    print(f"{i+1}: {val}")
+for i in book_texts:
+    print(get_full_tam_name(i))
+
+
